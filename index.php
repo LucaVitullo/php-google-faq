2cdf0e3bf0e4c4334d1dc2fb8c06d9a339fec2cf
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-05-04 16:14:23 +0200

    modificato l'array con tag interni

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<header>
+<header style="margin-bottom:50px">
     <img src="img/gughel-marchigiano-macerata-google.jpg" style="width: 20%;" alt="">
+    <span>Privacy e Termini</span>
 </header>
 
 <?php
@@ -9,4 +10,13 @@ include("Faqs.php");
 
 
 
+foreach ($Faqs as $key) {
+    echo $key['Domanda'];
+
+    if (isset($key['Domanda'])) {
+        $Domande    = $key['Domanda'];
+        $Risposte = $key['Risposta'];
+        echo $Risposte;
+    }
+}
 ?>
